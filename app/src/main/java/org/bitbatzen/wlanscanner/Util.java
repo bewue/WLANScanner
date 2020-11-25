910ffaec635b7d184f949d820d884aa8ad68f15d
bewue <>
2020-11-25 03:45:42 +0100

    update 5ghz channels

--- app/src/main/java/org/bitbatzen/wlanscanner/Util.java
+++ app/src/main/java/org/bitbatzen/wlanscanner/Util.java
@@ -19,12 +19,12 @@
 
 package org.bitbatzen.wlanscanner;
 
+import android.graphics.Color;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
-
-import android.graphics.Color;
 
 
 public class Util {
@@ -43,12 +43,12 @@ public class Util {
 	
     public static final Map<Integer, Integer> channels24GHzBand;
     public static final Map<Integer, Integer> channels5GHzBand;
-    
+
     public static final int START_24GHZ_BAND = 2412;
     public static final int END_24GHZ_BAND = 2484;
-    
+
     public static final int START_5GHZ_BAND = 4915;
-    public static final int END_5GHZ_BAND = 5825;
+    public static final int END_5GHZ_BAND = 5865;
     
     static {
     	Map<Integer, Integer> aMap = new HashMap<Integer, Integer>();
@@ -83,34 +83,60 @@ public class Util {
     	aMap.put(5055, 11);
     	aMap.put(5060, 12);
     	aMap.put(5080, 16);
+		aMap.put(5160, 32);
     	aMap.put(5170, 34);
-        aMap.put(5180, 36);
-        aMap.put(5200, 40);
-        aMap.put(5220, 44);
-        aMap.put(5240, 48);
-        aMap.put(5260, 52);
-        aMap.put(5280, 56);
-        aMap.put(5300, 60);
-        aMap.put(5320, 64);
-        aMap.put(5500, 100);
-        aMap.put(5520, 104);
-        aMap.put(5540, 108);
-        aMap.put(5560, 112);
-        aMap.put(5580, 116);
-        aMap.put(5600, 120);
-        aMap.put(5620, 124);
-        aMap.put(5640, 128);
-        aMap.put(5660, 132);
-        aMap.put(5680, 136);
-        aMap.put(5700, 140);
-        aMap.put(5745, 149);
-        aMap.put(5765, 153);
-        aMap.put(5785, 157);
-        aMap.put(5805, 161);
-        aMap.put(5825, 165);
+    	aMap.put(5180, 36);
+		aMap.put(5190, 38);
+		aMap.put(5200, 40);
+		aMap.put(5210, 42);
+		aMap.put(5220, 44);
+		aMap.put(5230, 46);
+		aMap.put(5240, 48);
+		aMap.put(5250, 50);
+		aMap.put(5260, 52);
+		aMap.put(5270, 54);
+		aMap.put(5280, 56);
+		aMap.put(5290, 58);
+		aMap.put(5300, 60);
+		aMap.put(5310, 62);
+		aMap.put(5320, 64);
+		aMap.put(5340, 68);
+		aMap.put(5480, 96);
+		aMap.put(5500, 100);
+		aMap.put(5510, 102);
+		aMap.put(5520, 104);
+		aMap.put(5530, 106);
+		aMap.put(5540, 108);
+		aMap.put(5550, 110);
+		aMap.put(5560, 112);
+		aMap.put(5570, 114);
+		aMap.put(5580, 116);
+		aMap.put(5590, 118);
+		aMap.put(5600, 120);
+		aMap.put(5610, 122);
+		aMap.put(5620, 124);
+		aMap.put(5630, 126);
+		aMap.put(5640, 128);
+		aMap.put(5660, 132);
+		aMap.put(5670, 134);
+		aMap.put(5680, 136);
+		aMap.put(5690, 138);
+		aMap.put(5700, 140);
+		aMap.put(5710, 142);
+		aMap.put(5720, 144);
+		aMap.put(5745, 149);
+		aMap.put(5755, 151);
+		aMap.put(5765, 153);
+		aMap.put(5775, 155);
+		aMap.put(5785, 157);
+		aMap.put(5795, 159);
+		aMap.put(5805, 161);
+		aMap.put(5825, 165);
+		aMap.put(5845, 169);
+		aMap.put(5865, 173);
         channels5GHzBand = Collections.unmodifiableMap(aMap);
     }
-	
+
 	public static FrequencyBand getFrequencyBand(int frequency) {
 		if (channels24GHzBand.containsKey(frequency)) {
 			return FrequencyBand.TWO_FOUR_GHZ;
@@ -161,6 +187,9 @@ public class Util {
 		}
 		if (capabilities.contains("WPA2")) {
 			shortString += "[WPA2]";
+		}
+		if (capabilities.contains("WPA3")) {
+			shortString += "[WPA3]";
 		}
 		if (capabilities.contains("WPS")) {
 			shortString += "[WPS]";
